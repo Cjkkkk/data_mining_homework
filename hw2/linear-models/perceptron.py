a3dbdad7b97b6c92cebbefdea8fc972d000b6b1c
cjkkkk <656569648@qq.com>
2019-05-21 00:39:40 +0800

    update hw2 perceptron

--- hw2/linear-models/perceptron.py
+++ hw2/linear-models/perceptron.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def perceptron(X, y):
     '''
     PERCEPTRON Perceptron Learning Algorithm.
@@ -12,11 +13,26 @@ def perceptron(X, y):
 
     '''
     P, N = X.shape
+    D = np.vstack((np.ones((1, N)), X))
     w = np.zeros((P + 1, 1))
     iters = 0
     # YOUR CODE HERE
-    
-    # begin answer
-    # end answer
-    
+    MAX_ITERATION = 5000
+    while iters < MAX_ITERATION:
+        flag = True
+        for i in range(N):
+            iters += 1
+            x = D[:, i].reshape((P + 1, 1))
+            label = np.sign(np.matmul(w.T, x))[0, 0]
+            if label != y[0, i]:
+                flag = False
+                w = w + x * y[0, i]
+        if flag:
+            break
+
+    # error = 0
+    # for i in range(N):
+    #     if predict[0, i] != y[0, i]:
+    #         error += (np.matmul(w.T, D[:, i].reshape((P + 1, 1)))[0, 0] * y[0, i])
+    # print(error)
     return w, iters
