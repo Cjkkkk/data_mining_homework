7c1c6be4aa1bd6fb6dd936a4a9a63a4fc3278b69
cjkkkk <656569648@qq.com>
2019-06-21 14:29:44 +0800

    add hw4 ans sheet

--- hw4/spectral_clustering/knn_graph.py
+++ hw4/spectral_clustering/knn_graph.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+# pointer = np.array([
+#     [1, 1],
+#     [2, 2],
+#     [3, 3],
+#     [4, 4],
+#     [5, 5]
+# ])
+
+
 def knn_graph(X, k, threshold):
     '''
     KNN_GRAPH Construct W using KNN graph
@@ -12,7 +21,17 @@ def knn_graph(X, k, threshold):
         Output:
             W - adjacency matrix, n-by-n matrix.
     '''
-
+    n, p = X.shape
+    W = np.zeros((n, n))
+    for i in range(n):
+        dis = [np.sum(np.square(X[i, :] - X[j, :])) for j in range(n)]
+        idx = np.argpartition(dis, k)[:k]
+        for j in idx:
+            W[i, j] = dis[j] if dis[j] > threshold else 0
+    return W
     # YOUR CODE HERE
     # begin answer
     # end answer
+
+
+# knn_graph(pointer, 4, 1)
--- hw4/spectral_clustering/spectral.py
+++ hw4/spectral_clustering/spectral.py
@@ -1,4 +1,5 @@
 import numpy as np
+import kmeans
 
 
 def spectral(W, k):
@@ -13,5 +14,22 @@ def spectral(W, k):
             idx: data point cluster labels, n-by-1 vector.
     '''
     # YOUR CODE HERE
+    n, _ = W.shape
     # begin answer
+    D = np.zeros((n, n))
+    for i in range(n):
+        D[i, i] = np.sum(W[i, :])
+
+    D_negative_sqrt = np.copy(D)
+    for i in range(n):
+        D_negative_sqrt[i, i] = 1 / np.sqrt(D[i, i])
+    A = np.matmul(np.matmul(D_negative_sqrt, D - W), D_negative_sqrt)
+    eigen_values, eigen_vectors = np.linalg.eig(A)
+    sorted_idx = np.argsort(eigen_values)
+    low = eigen_vectors[:, sorted_idx[1]]
+    # idx = np.random.randint(0, k, n)
+    # idx[z < 0] = 0
+    # idx[z > 0] = 1
+    # return idx
+    return kmeans.kmeans(low.reshape(n, 1), k)
     # end answer
